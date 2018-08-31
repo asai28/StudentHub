d8225a81cb14598ab93cfedbd4ce41def4f4e420
Anuhya Sai Nudurupati <anuhya.sai28@gmail.com>
2018-08-31 10:27:03 -0700

    Page Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Internships from "./Pages/Internships";
+import RateMyProfessor from "./Pages/RateMyProfessor";
+import Widgets from "./Pages/Widgets";
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+        <Router>
+        <div>
+            <Route exact path="/home" Component={Home} />
+            <Route exact path="/rmp" Component={RateMyProfessor} />
+            <Route exact path="/widgets" Component={Widgets} />
+            <Route exact path="/internships" Component={Internships} />
+        </div>
+        </Router>
+    )
+};
 
 export default App;
