389a9ab1a319148fe1527feb222c2cd1ede11e56
DutchMon <dutch.holland13@gmail.com>
2018-09-02 15:12:56 -0700

    can't get page to load currently, but should get it soon

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,32 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import Home from "./Pages/Home";
 // import Internships from "./Pages/Internships";
-// import RateMyProfessor from "./Pages/RateMyProfessor";
+import RateMyProfessor from "./Pages/RateMyProfessor/RateMyProfessor";
 // import Widgets from "./Pages/Widgets";
 import Navbar from "./Components/Navbar/index";
 import Container from "./Components/Container/index";
 
 const App = () => {
-    return (
-        <div>
-        <Navbar />
-        <div className="jumbotron text-center">
-        <h3>Everyone Successful Everyday!</h3>
-        </div>
-        <Container />
-        </div>
-    );      
+        <Router>
+            <div>
+                {/* <Route exact path="/home" Component={Home} /> */}
+                <Route exact path="/rmp" Component={RateMyProfessor} />
+                {/* <Route exact path="/widgets" Component={Widgets} /> */}
+                {/* <Route exact path="/internships" Component={Internships} /> */}
+            </div>
+        </Router>
+
+
 };
 
 export default App;
-    {/* <Router>
-        <div>
-        <Route exact path="/home" Component={Home} />
-        <Route exact path="/rmp" Component={RateMyProfessor} />
-        <Route exact path="/widgets" Component={Widgets} />
-        <Route exact path="/internships" Component={Internships} />
-    </div>
-    </Router>
- */}
+
+
+
+// <div>
+// <Navbar />
+// <div className="jumbotron text-center">
+// <h3>Everyone Successful Everyday!</h3>
+// </div>
+// <Container />
+// </div>
---
+++ client/src/Pages/RateMyProfessor/RateMyProfessor.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import { RMPform, Input, RMPbtn, FormGroup, Label } from "../../Components/Form";
+import Jumbotron from "../../Components/RMPjumbotron";
+import { H1, H3, H4 } from '../../Components/RMPheadings';
+import { Container, Row, Col } from "../../Components/RMPgrid";
+import { Panel, PanelHeading, PanelBody } from '../../Components/RMPpanel';
+
+export default class RMPforms extends Component {
+    state = {
+      firstName: '',//professor's first name
+      lastName: ''//professor's lastname
+
+    };
+  
+    //capturing state of inputs on change
+    handleInputChange = event => {
+      let { name, value } = event.target;
+      this.setState({[name] : value})
+    };
+  
+    //generating the query for the search from store state
+    handleFormSubmit = event => {
+      event.preventDefault();
+      let { firstName, lastName } = this.state;
+      let query = { firstName, lastName }
+      this.onSubmit(query)
+  
+    };
+  
+    //function that opens new tab with search for professor
+    onSubmit() { 
+  
+        let queryUrl =`http://www.ratemyprofessors.com/search.jsp?query=${this.state.firstName}+${this.state.lastName}`
+  
+      
+    //   open a new tab of the search results
+
+        window.open(queryUrl, "_blank")
+
+    }
+  
+    render() {
+      return (
+        <Container fluid>
+          <Row>
+            <Col size="sm-10" offset='sm-1'>
+              <Jumbotron>
+                <H1 className='page-header text-center'>Rate My Professor Professor Search</H1>
+                <H4 className='text-center'>Search for your professor by first and last name</H4>
+              </Jumbotron>
+              <Panel>
+                <PanelHeading>
+                  <H3>Search</H3>
+                </PanelHeading>
+                <PanelBody>
+                  <RMPform style={{marginBottom: '30px'}}>
+                    <FormGroup>
+                      <Label htmlFor="firstName">Enter your Professor's first name</Label>
+                      <Input
+                        onChange={this.handleInputChange}
+                        name='firstName'
+                        value={this.state.firstName}
+                        placeholder='First Name'
+                      />
+                    </FormGroup>
+                    <FormGroup >
+                      <Label htmlFor="lastName">Enter your Professor's last name</Label>
+                      <Input
+                        onChange={this.handleInputChange}
+                        name='lastName'
+                        value={this.state.lastName}
+                        placeholder='Last Name'
+                      />
+                    </FormGroup>
+                    {/* <FormGroup >
+                      <Label htmlFor="university">Enter your University</Label>
+                      <Input
+                        onChange={this.handleInputChange}
+                        name='universtiy'
+                        value={this.state.university}
+                        placeholder='University'
+                      />
+                    </FormGroup> */}
+                    <RMPbtn
+                      disabled={!(this.state.firstName && this.state.lastName)}
+                      onClick={this.handleFormSubmit}
+                      type='info'
+                      >Submit
+                    </RMPbtn>
+                  </RMPform>
+                </PanelBody>
+                </Panel>
+            </Col>
+          </Row>
+        </Container>
+      );
+    }
+  }
