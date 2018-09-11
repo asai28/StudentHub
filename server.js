e914df78c023d2f066a00ccc5664e4a0a6ef7835
DutchMon <dutch.holland13@gmail.com>
2018-09-11 13:08:48 -0700

    pls work

--- server.js
+++ server.js
@@ -37,14 +37,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // ==== if its production environment!
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  console.log("YOU ARE IN THE PRODUCTION ENV");
-  app.use("/static", express.static(path.join(__dirname, "../build/static")));
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build/"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   const path = require("path");
+//   console.log("YOU ARE IN THE PRODUCTION ENV");
+//   app.use("/static", express.static(path.join(__dirname, "../build/static")));
+//   app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../build/"));
+//   });
+// }
 
 /* Express app ROUTING */
 app.use("/auth", require("./auth"));
